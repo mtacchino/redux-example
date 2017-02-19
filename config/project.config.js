14e7bcb0e40f68503d5a6d1f363dce126d70790d
mtacchino <matt@tacchino.me>
2017-02-19 17:02:08 +0000

    omit linting in build

--- config/project.config.js
+++ config/project.config.js
@@ -17,7 +17,7 @@ const config = {
   path_base  : path.resolve(__dirname, '..'),
   dir_client : 'src',
   dir_dist   : 'dist',
-  dir_public : 'public',
+  dir_public : 'assets',
   dir_server : 'server',
   dir_test   : 'test',
 
